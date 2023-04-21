9a830646011a10eb95b94c42526b1f4ebfd303b6
Jacob McMullen <jacob.w.mcmullen@gmail.com>
2023-04-20 20:30:00 -0400

    prediction planning

--- predict.py
+++ predict.py
@@ -1,3 +1,5 @@
+#from functions import get_standings
+
 class Prediction:
     def __init__(self, id="", user="", historic_score={}, prediction={}):
         # string representing the mongo-assigned id
@@ -18,6 +20,32 @@ class Prediction:
 
         ## Class Invarient: 
         ## the sum of each each entry in prediction must equal the most recent entry of historic_score
+
+    # update_score(race)
+    # should only be called if current standings have changed, after a race
+    # compares standings to prediction, changing values along the way
+    # sum the new values of prediction
+    # adds a new value to historic score with key as (race) and entry as new prediction sum
+
+    # update_prediction
+    # abstracted out of update_score
+    # creates variable sum
+    # iterate through prediction alongside current standings
+    # if difference exist find out how far those offsets are and save them to prediction
+    # save each final prediction value to sum
+    # return sum
+
+    # get_curr_score
+    # if historic_score is empty, return 0
+    # else return the last value of historic score
+
+    # ranked_predictions
+    # create a new empty dict
+    # iterate for i in range len(prediction)
+    #   if len(dict) = len(prediction) return dict
+    # else insert any value from prediction that matches i
+    # 
+    # the returned dictionary will be the prediction in order of best to worst prediction
     
 
 # takes in a predition object, and returns a dictionary made from its state variables
@@ -26,7 +54,8 @@ def predict_to_dict(pred):
         raise TypeError("can only convert Prediction objects to dictionary")
     
     # first we convert our state variables into a dictionary
-    prediction_dictionary = dict(id=pred.id, user=pred.user, historic_score=pred.historic_score, prediction=pred.prediction)
+    # note depending on when this func is called keep in mind to not override the mongo _id, this might require further testing
+    prediction_dictionary = dict(_id=pred.id, user=pred.user, historic_score=pred.historic_score, prediction=pred.prediction)
 
     return prediction_dictionary
 
